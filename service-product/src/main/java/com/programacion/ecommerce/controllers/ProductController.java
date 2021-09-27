d4dd9c02597b71a3eb4535c01249408a17d5680c
samyhs20 <samy.hands99@hotmail.com>
2021-09-27 00:48:13 -0500

    Proceso del Modulo Producto

--- service-product/src/main/java/com/programacion/ecommerce/controllers/ProductController.java
+++ service-product/src/main/java/com/programacion/ecommerce/controllers/ProductController.java
@@ -2,19 +2,26 @@ package com.programacion.ecommerce.controllers;
 
 import java.util.List;
 
+import javax.annotation.security.RolesAllowed;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.json.JsonString;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.programacion.ecommerce.entities.ProductEntity;
+import com.programacion.ecommerce.entities.ReviewEntity;
 import com.programacion.ecommerce.services.ProductService;
 
+import org.eclipse.microprofile.jwt.Claim;
+import org.eclipse.microprofile.jwt.ClaimValue;
+
 @ApplicationScoped
-@Path("/")
+@Path("/products")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class ProductController {
@@ -22,11 +29,36 @@ public class ProductController {
   @Inject
   private ProductService productService;
 
+  @Inject
+  @Claim("custom-value")
+  private ClaimValue<JsonString> custom;
+
   @GET
-  @Path("/product")
   public List<ProductEntity> getAll() {
     List<ProductEntity> products = productService.getAll();
     return products;
+  }
+
+  @GET
+  @Path("/{id}")
+  public ProductEntity getOne(@PathParam("id") Integer id) {
+    ProductEntity product = productService.getOne(id);
+    return product;
+  }
+
+  @GET
+  @Path("/{id}/reviews")
+  public List<ReviewEntity> getReviews(@PathParam("id") Integer id) {
+    ProductEntity product = productService.getOne(id);
+    return product.getReviews();
+
+  }
+
+  @GET
+  @Path("/protected")
+  @RolesAllowed("user")
+  public String getJWTBasedValue() {
+    return "Protected Resource; Custom value : " + custom.getValue();
   }
 
 }
