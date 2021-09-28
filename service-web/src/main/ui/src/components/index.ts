4f3f3097bab617e3b5448cdccca2a2947dcff3f2
Ricardo Baquero <wola_ricky@hotmail.com>
2021-09-28 00:30:49 -0500

    - Prod ui added.

--- service-web/src/main/ui/src/components/index.ts
+++ service-web/src/main/ui/src/components/index.ts
@@ -1,6 +1,5 @@
 export * from "./Layout";
 export * from "./FormTextField";
-export * from "./Category";
 export * from "./ProductCard";
 export * from "./ReviewList";
 export * from "./ReviewForm";
